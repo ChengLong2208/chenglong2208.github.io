ba20f266fd1b5b9ab1d6d94d79c46566473f80c8
ykatyhoney <ykatyhoney@gmail.com>
2020-09-04 12:38:27 -0700

    Updates

---
+++ precache-manifest.068d1ed63f416b0286a9f830bd83e955.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1bfa32e4df430a275fd7e6ef5d9a0b",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "41b1b8270f73c9f0e1ef",
+    "url": "/resume/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "a4b202b81c23ec417d1d",
+    "url": "/resume/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "41b1b8270f73c9f0e1ef",
+    "url": "/resume/static/js/2.7ca6bb3b.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/resume/static/js/2.7ca6bb3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b202b81c23ec417d1d",
+    "url": "/resume/static/js/main.ce3d1f3d.chunk.js"
+  },
+  {
+    "revision": "88d66a0ce28414bf8bd8",
+    "url": "/resume/static/js/runtime-main.df5b4bf0.js"
+  },
+  {
+    "revision": "092a9d6a5591140da9727a50e483a92e",
+    "url": "/resume/static/media/bg5.092a9d6a.jpg"
+  },
+  {
+    "revision": "3dc450e9fc446cdbb41b747c3ec6025d",
+    "url": "/resume/static/media/bg7.3dc450e9.jpeg"
+  },
+  {
+    "revision": "b2d5337dcfa8e68cb356ef135d4696fa",
+    "url": "/resume/static/media/eventWebsite.b2d5337d.png"
+  },
+  {
+    "revision": "2144144fea9e5f37e664fc1941b863a8",
+    "url": "/resume/static/media/feedback1.2144144f.png"
+  },
+  {
+    "revision": "47f64d85f7f17127818d2965c537772d",
+    "url": "/resume/static/media/feedback2.47f64d85.jpg"
+  },
+  {
+    "revision": "c698c48f5bf2991e3759604613fdcda4",
+    "url": "/resume/static/media/feedback3.c698c48f.jpg"
+  },
+  {
+    "revision": "3140b193bcd7acf86b5c5d005971604c",
+    "url": "/resume/static/media/feedback4.3140b193.jpg"
+  },
+  {
+    "revision": "419644ca3e308f57a153ba897f53ec3f",
+    "url": "/resume/static/media/foodDelivery.419644ca.png"
+  },
+  {
+    "revision": "7980965fe4a6ce53722980ff21fa55e5",
+    "url": "/resume/static/media/foodDelivery2.7980965f.jpg"
+  },
+  {
+    "revision": "c02ec5955d7d78c8e5496d897a436b7d",
+    "url": "/resume/static/media/highQualityDesign.c02ec595.jpg"
+  },
+  {
+    "revision": "e4ce1a8614f368aaeca6e7bff1db5f28",
+    "url": "/resume/static/media/jewelry.e4ce1a86.jpg"
+  },
+  {
+    "revision": "a1e91feb5f50d85a66f1e0dbf283db53",
+    "url": "/resume/static/media/lifeStyleWebsite.a1e91feb.png"
+  },
+  {
+    "revision": "a768366c2195a0e69405355d1f004054",
+    "url": "/resume/static/media/mobile1.a768366c.jpg"
+  },
+  {
+    "revision": "9bb704cec53a4a5c9e2be0f73233deca",
+    "url": "/resume/static/media/mobile2.9bb704ce.png"
+  },
+  {
+    "revision": "d71427e3661fda46670f0fd12745fe88",
+    "url": "/resume/static/media/mobile3.d71427e3.jpg"
+  },
+  {
+    "revision": "a95a9f1be33193eda1c17a2cdbc92964",
+    "url": "/resume/static/media/onlineShoppingWebsite.a95a9f1b.jpg"
+  },
+  {
+    "revision": "f7139bdd86a5d7f745b6d46cb9834263",
+    "url": "/resume/static/media/person3.f7139bdd.jpg"
+  },
+  {
+    "revision": "061b5b6369ae8749303fbe3b13bf1430",
+    "url": "/resume/static/media/watchWebsite.061b5b63.jpg"
+  },
+  {
+    "revision": "0552f5ba2e234263df64e3c1eef43c42",
+    "url": "/resume/static/media/whyChooseUs.0552f5ba.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Individual_Website/precache-manifest.8103b1de08c9eeeaeb58432b122c19aa.js"
+  "/resume/precache-manifest.068d1ed63f416b0286a9f830bd83e955.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Individual_Website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
