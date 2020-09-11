972a0b25c82f87fdfdf0cfb749d7996fa452f7b9
ykatyhoney <ykatyhoney@gmail.com>
2020-09-11 14:19:08 -0700

    Updates

---
+++ precache-manifest.cc3e78035aba6982f99db232e802fcf3.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca283ae813535d63637a6e8cdc1640cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "888c7beb0111d3797bfd",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "da0fadf5faf9f28a656c",
+    "url": "/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "888c7beb0111d3797bfd",
+    "url": "/static/js/2.40f49b20.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/static/js/2.40f49b20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0fadf5faf9f28a656c",
+    "url": "/static/js/main.d206e04f.chunk.js"
+  },
+  {
+    "revision": "6616787f2a19acc0c886",
+    "url": "/static/js/runtime-main.e645a6af.js"
+  },
+  {
+    "revision": "46ef0f2f42982e8f579c89f6356a94bc",
+    "url": "/static/media/background1.46ef0f2f.png"
+  },
+  {
+    "revision": "092a9d6a5591140da9727a50e483a92e",
+    "url": "/static/media/bg5.092a9d6a.jpg"
+  },
+  {
+    "revision": "b2d5337dcfa8e68cb356ef135d4696fa",
+    "url": "/static/media/eventWebsite.b2d5337d.png"
+  },
+  {
+    "revision": "2144144fea9e5f37e664fc1941b863a8",
+    "url": "/static/media/feedback1.2144144f.png"
+  },
+  {
+    "revision": "47f64d85f7f17127818d2965c537772d",
+    "url": "/static/media/feedback2.47f64d85.jpg"
+  },
+  {
+    "revision": "c698c48f5bf2991e3759604613fdcda4",
+    "url": "/static/media/feedback3.c698c48f.jpg"
+  },
+  {
+    "revision": "3140b193bcd7acf86b5c5d005971604c",
+    "url": "/static/media/feedback4.3140b193.jpg"
+  },
+  {
+    "revision": "419644ca3e308f57a153ba897f53ec3f",
+    "url": "/static/media/foodDelivery.419644ca.png"
+  },
+  {
+    "revision": "7980965fe4a6ce53722980ff21fa55e5",
+    "url": "/static/media/foodDelivery2.7980965f.jpg"
+  },
+  {
+    "revision": "c02ec5955d7d78c8e5496d897a436b7d",
+    "url": "/static/media/highQualityDesign.c02ec595.jpg"
+  },
+  {
+    "revision": "e4ce1a8614f368aaeca6e7bff1db5f28",
+    "url": "/static/media/jewelry.e4ce1a86.jpg"
+  },
+  {
+    "revision": "a1e91feb5f50d85a66f1e0dbf283db53",
+    "url": "/static/media/lifeStyleWebsite.a1e91feb.png"
+  },
+  {
+    "revision": "a768366c2195a0e69405355d1f004054",
+    "url": "/static/media/mobile1.a768366c.jpg"
+  },
+  {
+    "revision": "9bb704cec53a4a5c9e2be0f73233deca",
+    "url": "/static/media/mobile2.9bb704ce.png"
+  },
+  {
+    "revision": "d71427e3661fda46670f0fd12745fe88",
+    "url": "/static/media/mobile3.d71427e3.jpg"
+  },
+  {
+    "revision": "a95a9f1be33193eda1c17a2cdbc92964",
+    "url": "/static/media/onlineShoppingWebsite.a95a9f1b.jpg"
+  },
+  {
+    "revision": "f7139bdd86a5d7f745b6d46cb9834263",
+    "url": "/static/media/person3.f7139bdd.jpg"
+  },
+  {
+    "revision": "061b5b6369ae8749303fbe3b13bf1430",
+    "url": "/static/media/watchWebsite.061b5b63.jpg"
+  },
+  {
+    "revision": "0552f5ba2e234263df64e3c1eef43c42",
+    "url": "/static/media/whyChooseUs.0552f5ba.png"
+  }
+]);
